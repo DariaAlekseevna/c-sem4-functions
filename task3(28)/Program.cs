66cc689a4b99159c52c51dc951fbb28758f9f82d
Daria Alekseevna <dasha.gugnina98@gmail.com>
2022-07-03 12:07:17 +0300

    решение задачи 4(30). массив из 8 рандомных 0 и 1.

--- task3(28)/Program.cs
+++ task3(28)/Program.cs
@@ -8,7 +8,28 @@ int n = 1;
 
 while (n <= N)
 {
-    Factorial = n * Factorial;
+    Factorial *= n;
     n++;
 }
 Console.WriteLine(Factorial);
+
+//через функцию
+
+System.Console.WriteLine("Введите число:");
+int N1 = int.Parse(Console.ReadLine());
+Console.WriteLine(FACTORIAL(N1));
+
+int FACTORIAL(int N1)
+{
+    int Factorial1 = 1;
+    int n1 = 1;
+
+    while (n1 <= N1)
+    {
+        Factorial1 *= n1;
+        n1++;
+    }
+    return Factorial1;
+}
+
+
