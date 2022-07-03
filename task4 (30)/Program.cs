9c5c0257f2653126758c36596292a45a078fb7ed
Daria Alekseevna <dasha.gugnina98@gmail.com>
2022-07-03 12:34:31 +0300

    решения с семинара

---
+++ task4 (30)/Program.cs
@@ -0,0 +1,30 @@
+﻿// выводит массив из 8 элементов, заполненный еденицами и нулями в случ. порядке.
+
+
+void FillArray(int[] array)
+{
+    int length = array.Length;
+    int index = 0;
+    while (index < length)
+    {
+        array[index] = new Random().Next(0, 2);
+        index++;
+    }
+
+}
+
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(array[position]);
+        position++;
+    }
+}
+
+int[] array = new int [8]; //определили массив из 8 эл-тов
+
+FillArray(array); //вызвали функцию заполнения массива
+PrintArray(array); //вызвали функцию печати массива
